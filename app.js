1cb17b27c709f3713b2e9d09d261c987194436fe
jakelthedeveloper <jakelj24@gmail.com>
2020-10-21 23:32:15 -0700

    complete update

--- app.js
+++ app.js
@@ -9,24 +9,45 @@ app.use(morgan('dev'));
 
 
 app.get('/app', (req,res)=>{
-    const {search} = req.query;
+    const {sort = '', genres = ''} = req.query;
+    const availGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card']
     let data = [...appData]
     
-    if(search && search !== "rating" && search !== "app"){
-        return res.status(400).json({message:'must be "rating" or "App"'})
-    }
-        if(search){
-            if(Rating )
-         data.sort((current,next)=>{
-                if(current.Rating < next.Rating) return -1
-               if(next.Rating > current.Rating) return 1
-               return 0
-           })
+     if(sort && sort !== "rating" && sort !== "app"){
+         return res.status(400).json({message:'must be "rating" or "App"'})
      }
+        if(sort){
+            if(sort === "rating"){
+                 data.sort((a,b)=>{
+                    return a.Rating > b.Rating ? 1 : a.Rating < b.Rating ? -1 : 0
+                 })
+            }
+        }
+            if(sort){
+                if(sort === "app"){
+                    data.sort((a,b)=>{
+                        return a.App > b.App ? 1: a.App < b.App ? -1 : 0
+                })
+            }
+        }
+
+     let match = false;
+     let result = '';
+    
+    for(let i = 0;i<availGenres.length;i++){
+        if(genres.toLowerCase() === availGenres[i].toLowerCase()){
+            match = true;
+            result = availGenres[i];
+        }
+    }
+        if(genres && !match){
+            return res.status(400).json({message:'must be of available genre type [Action, Puzzle, Strategy, Casual, Arcade, Card]'})
+        }
+        if(genres){
+             data = data.filter(item => item.Genres === result);
+        }
     res.json(data)
 })
-
-
 
 app.listen(8080, ()=> {console.log('server on 8080')})
 
